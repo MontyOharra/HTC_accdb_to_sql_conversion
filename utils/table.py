79cd194e9bc0019504c2906583e775f53ac626cb
Ethan Harrah <em.harrah.business@gmail.com>
2024-09-25 14:55:47 -0500

    add more error handling stuff

--- utils/table.py
+++ utils/table.py
@@ -1,12 +1,14 @@
+from typing import Dict, Any
+
 class Table():
-    def __init__(self, tableName, sqlConn):
+   def __init__(self, tableName, sqlConn):
         self.tableName = tableName
         self.sqlConn = sqlConn
         self.sqlCursor = sqlConn.cursor()
 
         self.currentProcess = ""
 
-    def drop(self):
+   def drop(self):
         try:
           self.currentProccess = "droppingTable"
           dropSql = f"DROP TABLE IF EXISTS [{self.tableName}]"
@@ -14,27 +16,57 @@ class Table():
         except:
            self.handleError()
 
-
-    def create(self, tableFields):
+   def createTable(self, tableFields):
         try:
+            self.currentProccess = "creatingTable"
             createSql = f"CREATE TABLE [{self.tableName}] ({tableFields})"
             self.sqlCursor.execute(createSql)
         except:
            self.handleError()
 
-    def commit(self):
+   def addRow(self, fields : Dict[str, Any]):
+        try:   
+            self.currentProccess = "insertingRow"
+            fieldNames = list(fields.keys())
+            fieldNamesString = ', '.join(fieldNames).rstrip(', ')
+            
+            fieldValues = list(fields.items())
+            fieldValuesString = ', '.join(fieldValues).rstrip(', ')
+            insertSql = f"INSERT INTO [{self.tableName}] ({fieldNamesString}) VALUES ({fieldValuesString})"
+            
+            self.sqlCursor.execute(insertSql)
+        except:
+           self.handleError(fields)
+           
+   def addForeignKey(self, thisTableField, foreignTableName, foreignTableField):
+      try:
+         self.currentProccess = "addingForeignKey"
+         addForeignKeySql = f"ALTER TABLE [{self.tableName}] ADD FOREIGN KEY ([{thisTableField}]) REFERENCES [{foreignTableName}] ([{foreignTableField}])"
+         self.sqlCursor.execute(addForeignKeySql)
+      except Exception as err:
+         self.handleError([foreignTableName, foreignTableField])
+         
+        
+
+   def commit(self):
         try:
           self.sqlConn.commit()
         except:
            self.handleError()
+           
 
-    def handleError(self):
-       print(f"There was an error in the {self.tableName} table.")
-       if (self.currentProcess == ""):
-          errorMessage = "    [Error initializing table object.]"
-       elif (self.currentProcess == "droppingTable"):
-          errorMessage = "    [Error dropping the table.]"
-       elif (self.currentProcess == "creatingTable"):
-          errorMessage = "    [Error dropping the table.]"
-       elif (self.currentProcess == "droppingTable"):
-          errorMessage = "    [Error dropping the table.]"
+   def handleError(self, errorDetail, info = None):
+      if (self.currentProcess == ""):
+         errorMessage = "    [Error initializing table object.]"
+      elif (self.currentProcess == "droppingTable"):
+         errorMessage = "    [Error dropping the table.]"
+      elif (self.currentProcess == "creatingTable"):
+         errorMessage = "    [Error dropping the table.]"
+      elif (self.currentProcess == "insertingRow"):
+         errorMessage = "    [Error inserting row.] Details:"
+         errorMessage += "\n"
+         for fieldName, fieldValue in info.items():
+            errorMessage += f"        Name: [{fieldName}], Value: [{fieldValue}], Type: [{type(fieldValue)}]"
+            
+      print(f"There was an error in the {self.tableName} table:\n")
+      print(errorMessage)
