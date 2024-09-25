79c0a93f510c3c5dae46c2bbad73a54132d8056a
Ethan Harrah <em.harrah.business@gmail.com>
2024-09-24 22:43:55 -0500

    Start adding error handler on table object

--- utils/table.py
+++ utils/table.py
@@ -4,16 +4,37 @@ class Table():
         self.sqlConn = sqlConn
         self.sqlCursor = sqlConn.cursor()
 
+        self.currentProcess = ""
+
     def drop(self):
         try:
+          self.currentProccess = "droppingTable"
           dropSql = f"DROP TABLE IF EXISTS [{self.tableName}]"
           self.sqlCursor.execute(dropSql)
         except:
-          print()
+           self.handleError()
+
 
     def create(self, tableFields):
-        createSql = f"CREATE TABLE [{self.tableName}] ({tableFields})"
-        self.sqlCursor.execute(createSql)
+        try:
+            createSql = f"CREATE TABLE [{self.tableName}] ({tableFields})"
+            self.sqlCursor.execute(createSql)
+        except:
+           self.handleError()
 
     def commit(self):
-        self.sqlConn.commit()
+        try:
+          self.sqlConn.commit()
+        except:
+           self.handleError()
+
+    def handleError(self):
+       print(f"There was an error in the {self.tableName} table.")
+       if (self.currentProcess == ""):
+          errorMessage = "    [Error initializing table object.]"
+       elif (self.currentProcess == "droppingTable"):
+          errorMessage = "    [Error dropping the table.]"
+       elif (self.currentProcess == "creatingTable"):
+          errorMessage = "    [Error dropping the table.]"
+       elif (self.currentProcess == "droppingTable"):
+          errorMessage = "    [Error dropping the table.]"
